5124b9bc590dc6743546d2546aa0b5e5253140dc
Aviksaikat <saikickkarma@gmail.com>
2021-11-17 12:34:46 +0530

    update & installation script separated

--- get-tools.sh
+++ get-tools.sh
@@ -1,74 +1,15 @@
 #!/bin/bash
 #! Bug bounty essential tools install
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
-if ! command -v lolcat &> /dev/null
-then
-    echo "lolcat could not be found...." 
-    echo "Installing lolcat for fun...."
-    sudo apt install -y lolcat
-fi
-
-echo "Updating system...." | lolcat
-sudo apt-get update -y && sudo apt-get upgrade -y
-
-if ! command -v go &> /dev/null
-then
-    echo "go could not be found...." | lolcat
-    echo "Installing golang...." | lolcat
-    sudo apt install -y golang
-    echo "Setting go...." | lolcat
-    mkdir -p "HOME/go"
-    
-    if [[ $(ps -p $$ | tail -1 | awk '{print $4}') -eq "zsh" ]];
-    then 
-        echo 'export PATH="PATH:HOME/go/bin"' >> $HOME/.zshrc
-    fi
-    
-    if [[ $(ps -p $$ | tail -1 | awk '{print $4}') -eq "bash" ]];
-    then
-        echo 'export PATH="PATH:HOME/go/bin"' >> $HOME/.bashrc
-    else
-        echo "Enter the location of your shell file: "
-        read p
-        echo 'export PATH="PATH:HOME/go/bin"' >> $p 
-    fi
-fi
-
-mkdir installation
-cd installation
-
-#* from repo
-sudo apt-get install -y python3-pip
-sudo apt-get install -y cargo 
-sudo apt-get install -y libpcap-dev
-sudo apt-get install -y amass sublist3r subfinder assetfinder altdns dirsearch feroxbuster ffuf gobsuter dirb dirbuster nmap 
-
-#* from pip
-pip3 install arjun
-pip3 install wfuzz
-pip3 install -r requirements.txt --user
-pip3 install xsrfprobe
-sudo pip3 install pycurl bs4 pygeoip gobject cairocffi selenium
-
-
-#? rustscan
-# wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb 
-
-# sudo dpkg -i *.deb
-
 get_from_git()
 {
     echo "Getting findomain...." | lolcat
-    git clone https://github.com/findomain/findomain.git -C /tmp
-    cd /tmp/findomain
+    git clone https://github.com/findomain/findomain.git 
+    cd findomain
+    #./builder.sh
     cargo build --release
     sudo cp target/release/findomain /usr/bin/
-    findomain
+    #findomain
     cd ..
     
     echo "Getting knockpy...." | lolcat
@@ -119,7 +60,7 @@ get_from_git()
 get_from_git
 
 
-go_modules()
+go_mods()
 {
     echo "Installing go modules...." | lolcat
     go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
@@ -130,24 +71,25 @@ go_modules()
     go install github.com/hakluke/hakrawler@latest
     go install github.com/lc/gau/v2/cmd/gau@latest
     go get -u github.com/iamstoxe/urlgrab
-    go get github.com/003random/getJS
-    go get github.com/ameenmaali/qsfuzz
+    go get -u github.com/003random/getJS
+    go get -u github.com/ameenmaali/qsfuzz
     GO111MODULE=on go get -v github.com/dwisiswant0/crlfuzz/cmd/crlfuzz
     go install github.com/hahwul/dalfox/v2@latest
-    go get github.com/haccer/subjack
-    go get github.com/Ice3man543/SubOver
+    go get -u github.com/haccer/subjack
+    go get -u github.com/Ice3man543/SubOver
     go get -u github.com/tomnomnom/anew
     go get -u github.com/tomnomnom/meg
-    go get github.com/tomnomnom/waybackurls
+    go get -u github.com/tomnomnom/waybackurls
     go get -u github.com/tomnomnom/qsreplace
     go get -u github.com/tomnomnom/gf
     go get -u github.com/tomnomnom/httprobe
     go get -u github.com/tomnomnom/concurl
-    go get github.com/tomnomnom/burl
+    go get -u github.com/tomnomnom/burl
     go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
     go install github.com/hakluke/hakrawler@latest
     GO111MODULE=on go get -u -v github.com/bp0lr/gauplus
     go get -u github.com/1ndianl33t/urlprobe
 
 }
-go_modules
+go_mods
+go_mods
---
+++ update-packages.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#! Bug bounty essential tools install
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if ! command -v lolcat &> /dev/null
+then
+    echo "lolcat could not be found...." 
+    echo "Installing lolcat for fun...."
+    sudo apt install -y lolcat
+fi
+
+echo "Updating system...." | lolcat
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+if ! command -v go &> /dev/null
+then
+    echo "go could not be found...." | lolcat
+    echo "Installing golang...." | lolcat
+    sudo apt install -y golang
+    echo "Setting go...." | lolcat
+    mkdir -p "$HOME/go"
+    
+    if [[ $(ps -p $$ | tail -1 | awk '{print $4}') -eq "zsh" ]];
+    then 
+        echo 'export GOPATH=$HOME/go' >> $HOME/.zshrc
+        echo 'export PATH="$PATH:$GOPATH/bin"' >> $HOME/.zshrc
+    fi
+    
+    if [[ $(ps -p $$ | tail -1 | awk '{print $4}') -eq "bash" ]];
+    then
+        echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
+        echo 'export PATH="$PATH:$GOPATH/bin"' >> $HOME/.bashrc
+    else
+        echo "Enter the location of your shell file: "
+        read p
+        echo 'export PATH="$PATH:$HOME/go/bin"' >> $p 
+    fi
+fi
+
+mkdir installation
+cd installation
+
+#* from repo
+sudo apt-get install -y python3-pip
+sudo apt-get install -y cargo 
+sudo apt-get install -y libpcap-dev
+sudo apt-get install -y amass sublist3r subfinder assetfinder altdns dirsearch feroxbuster ffuf gobuster dirb dirbuster nmap 
+
+#* from pip
+pip3 install arjun
+pip3 install wfuzz
+pip3 install xsrfprobe
+sudo pip3 install pycurl bs4 pygeoip gobject cairocffi selenium
+
+
+#? rustscan
+# wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb 
+
+# sudo dpkg -i *.deb
